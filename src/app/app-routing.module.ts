a65ad73074a9ee61f5dc07caaed9ced5913cb73f
Jorge Calderón <Jorgedcalderon1989@gmail.com>
2022-10-30 23:02:21 -0300

    PUTO EL QUE LEE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ChatComponent } from './pages/chat/chat.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [];
@@ -18,6 +19,7 @@ const routes: Routes = [];
     {path: 'sobre-mi', component: AboutmeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'mi-perfil', component: ProfileComponent},
     {path: '**', component: ErrorComponent},
   ])],
   exports: [RouterModule]
